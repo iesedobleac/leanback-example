066aba10ef372df6952ced10083096d9696d9634
Isaac de los Reyes <isaacdelosredi@gmail.com>
2022-11-22 01:12:13 +0100

    Se añade el fragmento de ajustes usando la clase de Preferencias que nos brinda Leanback que no es más que la de Android pero adaptada, con toda la explicación y documentación para su mejor comprensión.

--- app/src/main/java/com/isaacdelosreyes/googletvleanback/utils/Constants.kt
+++ app/src/main/java/com/isaacdelosreyes/googletvleanback/utils/Constants.kt
@@ -6,3 +6,5 @@ const val DISCOVER_SORT_BY_LEAST_VOTED = "vote_count.asc"
 const val DISCOVER_SORT_BY_REVENUE = "revenue.desc"
 
 const val KEY_MOVIE_SELECTED = "MOVIE_SELECTED"
+
+const val KEY_ABOUT_DEV_GOOGLE_PLAY_SETTINGS = "prefs_about_developer_google_play"
